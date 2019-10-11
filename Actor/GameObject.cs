a73f2d3f0e60e375cc43be03187dc7a406df1bf1
syake <->
2019-10-11 15:02:57 +0900

    Actorフォルダの中身を追加 Player,GameObject,GameObjectManager,Map,Space,Block Deviceフォルダ CSVReaderの追加 Texture: playerの仮画像 blockの仮画像

---
+++ Actor/GameObject.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Toss.Device;
+
+namespace Toss.Actor
+{
+
+    //当たった方向
+    enum Direction
+    {
+        Top, Bottom, Right, Left
+    }
+    abstract class GameObject
+    {
+
+        protected string name;
+        protected Vector2 position;
+        protected int width;
+        protected int height;
+        protected bool isDeadFlag;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="position"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public GameObject(string name, Vector2 position, int width, int height)
+        {
+            this.name = name;
+            this.position = position;
+            this.width = width;
+            this.height = height;
+        }
+
+        /// <summary>
+        /// 位置の設定
+        /// </summary>
+        /// <param name="position"></param>
+        public void SetPosition(Vector2 position)
+        {
+            this.position = position;
+        }
+        /// <summary>
+        /// 位置の取得
+        /// </summary>
+        /// <returns></returns>
+        public Vector2 GetPosition()
+        {
+            return position;
+        }
+        /// <summary>
+        /// オブジェクト幅の取得
+        /// </summary>
+        /// <returns></returns>
+        public int GetWidth()
+        {
+            return width;
+        }
+        /// <summary>
+        /// オブジェクトの高さの取得
+        /// </summary>
+        /// <returns></returns>
+        public int GetHeight()
+        {
+            return height;
+        }
+        //抽象メソッド
+        public abstract void Update(GameTime gameTime);   //更新
+        public abstract void Hit(GameObject gameObject);  //ヒット通知
+
+        //矩形同士の当たり判定
+        public bool IsCollusion(GameObject otherObj)
+        {
+            float sabunX = this.position.X - otherObj.position.X;
+            if (sabunX < this.width + otherObj.width) return true;
+            float sabunY = this.position.Y - otherObj.position.Y;
+            if (sabunY < this.width + otherObj.width) return true;
+
+            return false;
+
+        }
+
+        //仮想メソッド
+        /// <summary>
+        /// 描画
+        /// </summary>
+        /// <param name="renderer">描画オブジェクト</param>
+        public virtual void Draw(Renderer renderer)
+        {
+            renderer.DrawTexture(name, position);
+        }
+        //矩形の生成
+        public Rectangle GetRectangle()
+        {
+            //短形の生成
+            Rectangle area = new Rectangle();
+
+            //位置と幅、高さを設定
+            area.X = (int)position.X;
+            area.Y = (int)position.Y;
+            area.Height = height;
+            area.Width = width;
+            return area;
+        }
+        //どこの辺が当たっているか
+        public Direction CheckDirection(GameObject otherObj)
+        {
+            //中心位置の取得
+            Point thisCenter = this.GetRectangle().Center; 　　   //自分の中心位置
+            Point otherCenter = otherObj.GetRectangle().Center;   //相手の中心位置
+
+            //向きのベクトルを取得
+            Vector2 dir = new Vector2(thisCenter.X, thisCenter.Y) -
+                          new Vector2(otherCenter.X, otherCenter.Y);
+
+            //当たっている側面をリターンする
+            //x成分とy成分でどちらの方が量が多いか
+            if (Math.Abs(dir.X) > Math.Abs(dir.Y))
+            {
+                //xの向きが正の時
+                if (dir.X > 0)
+                {
+                    return Direction.Right;
+                }
+                return Direction.Left;
+            }
+            //y成分が大きく、正の値か？
+            if (dir.Y > 0)
+            {
+                return Direction.Bottom;
+            }
+            //プレイヤーがブロックに乗った
+            return Direction.Top;
+        }
+
+        /// <summary>
+        /// 当たった面からの位置補正
+        /// </summary>
+        /// <param name="other"></param>
+        public virtual void CorrectPosition(GameObject other)
+        {
+            //当たった面の取得
+            Direction dir = this.CheckDirection(other);
+            
+            switch (dir)
+            {
+                case Direction.Top:
+                    position.Y = other.GetRectangle().Top - this.height;
+                    break;
+                case Direction.Bottom:
+                    position.Y = other.GetRectangle().Bottom;
+                    break;
+                case Direction.Right:
+                    position.X = other.GetRectangle().Right;
+                    break;
+                case Direction.Left:
+                    position.X = other.GetRectangle().Left - this.width;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ Actor/Map.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Toss.Device;
+using Toss.Util;
+
+namespace Toss.Actor
+{
+    class Map
+    {
+        private List<List<GameObject>> mapList;
+        private GameDevice gameDevice;
+
+        public Map(GameDevice gameDevice)
+        {
+            mapList = new List<List<GameObject>>();
+            this.gameDevice = gameDevice;
+        }
+        private List<GameObject> addBlock(int lineCnt, string[] line)
+        {
+            Dictionary<string, GameObject> objectDict = new Dictionary<string, GameObject>();
+
+            List<GameObject> workList = new List<GameObject>();
+            int colCnt = 0;
+            foreach (var s in line)
+            {
+                try
+                {
+                    //ディクショナリから元データ取り出し、クローン機能で複製
+                    GameObject work = (GameObject)objectDict[s];
+                    work.SetPosition(new Vector2(colCnt * work.GetHeight(), lineCnt * work.GetWidth()));
+                    workList.Add(work);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                colCnt += 1;
+
+            }
+            return workList;
+            
+        }
+        /// <summary>
+        /// CSVReaderを使ってMapの読み込み
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="path"></param>
+        public void Load(string filename, string path = "./")
+        {
+            CSVReader csvReader = new CSVReader();
+            csvReader.Read(filename, path);
+
+            var data = csvReader.GetData();    //List<string[]>型で取得
+
+            //1行ごとにmapListに追加していく
+            for (int lineCnt = 0; lineCnt < data.Count(); lineCnt++)
+            {
+                mapList.Add(addBlock(lineCnt, data[lineCnt]));
+            }
+        }
+
+
+
+        /// <summary>
+        /// マップリストのクリア
+        /// </summary>
+        public void Unoad()
+        {
+            mapList.Clear();
+        }
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public void Update(GameTime gameTime)
+        {
+            foreach (var list in mapList)　　　　　//ListはList<GameObject>型
+            {
+                foreach (var obj in list)          //objはGameObject型
+                {
+                    //objがSpaceクラスのオブジェクトなら次へ
+                    if (obj is Space)
+                    {
+                        continue;
+                    }
+                    //更新
+                    obj.Update(gameTime);
+                }
+            }
+        }
+        public void Hit(GameObject gameObject)
+        {
+            Point work = gameObject.GetRectangle().Location;      //左上の座標くぉ取得
+                                                                  //配列の何行何列目にいるかを計算
+            int x = work.X / 32;
+            int y = work.Y / 32;
+            //移動で食い込んでいる時の修正
+            if (x < 1)
+            {
+                x = 1;
+            }
+            if (y < 1)
+            {
+                y = 1;
+            }
+
+            Range yRange = new Range(0, mapList.Count() - 1);
+            Range xRange = new Range(0, mapList[0].Count() - 1);
+
+            for (int row = y - 1; row <= (y + 1); row++)
+            {
+                for (int col = x - 1; col <= (x + 1); col++)
+                {
+                    //配列外にないなら何もしない
+                    if (xRange.IsOutOfRange(col) || yRange.IsOutOfRange(row))
+                    {
+                        continue;
+                    }
+
+                    //その場所のオブジェクトを取得
+                    GameObject obj = mapList[row][col];
+                    //objがSpaceクラスのオブジェクトなら次へ
+                    if (obj is Space)
+                    {
+                        continue;
+                    }
+                    //衝突判定
+                    if (obj.IsCollusion(gameObject))
+                    {
+                        gameObject.Hit(obj);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 描画
+        /// </summary>
+        /// <param name="renderer">描画オブジェクト</param>
+        public void Draw(Renderer renderer)
+        {
+            foreach (var m in mapList)
+            {
+                foreach (var ml in m)
+                {
+                    ml.Draw(renderer);
+                }
+            }
+        }
+
+
+        public int GetWidth()
+        {
+            int col = mapList[0].Count;
+            int width = col * mapList[0][0].GetWidth();
+            return width;
+        }
+        public int GetHeight()
+        {
+            int row = mapList[0].Count;
+            int height = row * mapList[0][0].GetHeight();
+            return height;
+        }
+    }
+}
