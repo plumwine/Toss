01a79c8ec6114b56a321f579b6f88af1dc9a01bb
syunpei <ogasyun821@gmail.com>
2019-10-15 09:27:11 +0900

    パーティクルクラス追加

---
+++ Actor/Effects/IParticleMediatorcs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toss.Actor.Effects
+{
+   
+        ///<summary>
+        ///パーティクル仲介者
+        /// </summary>
+        interface IParticleMediator
+        {
+            ///<summary>
+            /// 生成
+            /// </summary>
+            /// <param name="name">パーティクル名</param>
+            ///<returns>実体生成されたパーティクル</returns>
+            Particle generate(string name);
+        }
+   
+}
